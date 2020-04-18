2e7a058d07f4088d088773162aa173f49d6176d2
AQ-TeppeiKikuchi <teppei.kikuchi@asia-quest.co.jp>
2020-04-18 14:24:34 +0900

    デバッグラベルの削除

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: SafeArea(
           child: PaperScreen(),
         ),
