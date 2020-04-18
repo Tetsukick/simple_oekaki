0e373aee45a9c0ab78aa2baea4036b4f6dbe33dc
AQ-TeppeiKikuchi <teppei.kikuchi@asia-quest.co.jp>
2020-04-18 13:37:54 +0900

    画面二分割

--- lib/screens/paper_screen.dart
+++ lib/screens/paper_screen.dart
@@ -5,47 +5,30 @@ import 'package:simple_oekaki/components/palette.dart';
 import 'package:simple_oekaki/components/paper.dart';
 import 'package:simple_oekaki/models/strokes_model.dart';
 
+import '../components/paper.dart';
+
 class PaperScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final strokes = Provider.of<StrokesModel>(context);
+    final Size size = MediaQuery.of(context).size;
 
     return Scaffold(
-      body: Stack(
+      body: Column(
         children: <Widget>[
-          Paper(),
-          Align(
-            alignment: Alignment.topLeft,
-            child: Palette(),
+          Container(
+            height: size.height / 2,
+            child: Paper(),
           ),
+          Expanded(
+            child: Paper(),
+          )
         ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.delete),
         onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                title: Text('削除します'),
-                actions: <Widget>[
-                  FlatButton(
-                    child: Text('Cancel'),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                  FlatButton(
-                    child: Text('OK'),
-                    onPressed: () {
-                      strokes.clear();
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                ],
-              );
-            },
-          );
+          strokes.clear();
         },
       ),
     );
